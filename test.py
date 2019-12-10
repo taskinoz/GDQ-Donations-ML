80c411b2188b5e677a8bcd0eb01a901897c16eb9
taskinoz <tristan@taskinoz.com>
2019-12-10 14:42:34 +1100

    Added source and export directories

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ from textgenrnn import textgenrnn
 import os
 
 textgen = textgenrnn()
-textgen.train_from_file('gdqtext-3000length.txt', num_epochs=10)
-textgen.generate_to_file('donations3.txt', n=20, temperature=0.5)
+textgen.train_from_file('js-out/gdqtext-8000length.txt', num_epochs=10)
+textgen.generate_to_file('py-out/donations4.txt', n=20, temperature=0.5)
