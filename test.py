ff0ceb89fe741057689c39449e302aecb56e6e8f
taskinoz <tristan@taskinoz.com>
2019-12-11 15:11:41 +1100

    Added more comments

--- test.py
+++ test.py
@@ -4,6 +4,9 @@ import os
 # num_epochs - Time training on data
 # temperature - Confidence in its generated text
 # n - Number of times to generate
+# interactive - allows you to train the model by selecting the words
+# top_n - Top n choices used by interactive mode
+# weights_path - Specify weights path
 
 textgen = textgenrnn(weights_path='textgenrnn_weights.hdf5')
 textgen.train_from_file('js-out/gdqtext-8000length.txt', num_epochs=10)
