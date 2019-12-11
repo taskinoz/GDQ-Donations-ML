0c1fe40e68a1ed1c898819602187708092cffc19
taskinoz <tristan@taskinoz.com>
2019-12-11 13:48:07 +1100

    Added comments for configs

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 from textgenrnn import textgenrnn
 import os
 
+# num_epochs - Time training on data
+# temperature - Confidence in its generated text
+# n - Number of times to generate
+
 textgen = textgenrnn()
 textgen.train_from_file('js-out/gdqtext-8000length.txt', num_epochs=10)
 textgen.generate_to_file('py-out/donations4.txt', n=20, temperature=0.5)
