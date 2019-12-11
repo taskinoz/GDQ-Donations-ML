276e5aae76ef81ca983f57216cf2a4f0f15d21ce
taskinoz <tristan@taskinoz.com>
2019-12-11 15:10:33 +1100

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ import os
 # temperature - Confidence in its generated text
 # n - Number of times to generate
 
-textgen = textgenrnn()
+textgen = textgenrnn(weights_path='textgenrnn_weights.hdf5')
 textgen.train_from_file('js-out/gdqtext-8000length.txt', num_epochs=10)
 textgen.generate_to_file('py-out/donations4.txt', n=20, temperature=0.5)
