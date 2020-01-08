cfc6ad17616895e178e5afe818acdd8f65adba5a
taskinoz <tristan@taskinoz.com>
2020-01-09 01:23:57 +1100

    Changed learning target

--- test.py
+++ test.py
@@ -9,5 +9,5 @@ import os
 # weights_path - Specify weights path
 
 textgen = textgenrnn(weights_path='textgenrnn_weights.hdf5')
-textgen.train_from_file('js-out/gdqtext-random.txt', num_epochs=10)
-textgen.generate_to_file('py-out/donations-random1.txt', n=1000, temperature=0.5)
+textgen.train_from_file('js-out/scraped.txt', num_epochs=10)
+textgen.generate_to_file('py-out/scrapedPy.txt', n=1000, temperature=0.5)
